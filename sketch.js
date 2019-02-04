a8d1327d31cb61742ee3f236f73ceb6fe0b20155
Mikołaj Zuziak <miki.zuziak@gmail.com>
2019-02-04 19:23:07 +0100

    commit

--- sketch.js
+++ sketch.js
@@ -1,11 +1,9 @@
 class Segment {
-    constructor(x, y, color, stat, bound, inside) {
+    constructor(x, y, color, stat) {
         this.x = x;
         this.y = y;
         this.color = color
         this.stat = stat;
-        this.bound = bound;
-        this.inside = inside;
         this.limit = 5;
         this.num = 0;
     }
@@ -21,15 +19,11 @@ class Segment {
             textAlign(CENTER, CENTER);
             textSize(24);
             fill(0);
-            stroke(255);
+            stroke(0);
             text(this.num, 3, 3, size, size);
         }
 
         pop();
-    }
-
-    action() {
-        this.increaseNum();
     }
 
     checkPointing() {
@@ -50,72 +44,16 @@ class Segment {
 
 }
 
-class Btn {
-    constructor(x, y, txt, act) {
-        this.x = x;
-        this.y = y;
-        this.txt = txt;
-        this.bound = false;
-        this.inside = false;
-        this.act = act;
-    }
-
-    display() {
-        push();
-        translate(this.x * size, this.y * size);
-
-        fill(255);
-        stroke(120);
-        rect(0, 0, size, size, 5, 5, 5, 5)
-
-        fill(0);
-        textAlign(CENTER, CENTER)
-        text(this.txt, 1, 1, size, size);
-
-        pop();
-    }
-
-    action() {
-        if (this.act == 'o') {
-            resetBounds();
-        } else if (this.act == 'p') {
-            resetInside();
-        }
-    }
-
-    checkPointing() {
-
-        if (mouseX > this.x * size && mouseX < this.x * size + size &&
-            mouseY > this.y * size && mouseY < this.y * size + size) {
-            return true;
-        } else {
-            return false;
-        }
-
-    }
-}
-
 let segments = [],
     bd = [],
     ins = [];
 
-let bound = [2, 3, 4, 5, 7, 12, 13, 18, 19, 24, 25, 30, 31, 32, 33, 34, 35];
+let bound = [2, 3, 4, 5, 31, 32, 33, 34, 35, 7, 13, 19, 25, 12, 18, 24, 30, 18];
 let skalar;
 let colors;
 let size;
-let n = 1;
-
-function createMenu() {
-    skalar = createSlider(60, 140, 60, 10);
-    select('.scale').child(skalar);
-    skalar.changed(resize);
-}
-
-function resize() {
-    size = skalar.value();
-    print('done!');
-    resizeCanvas(size * 6, size * 6)
-}
+let n = 1,
+    z;
 
 function resetBounds() {
     for (b of bd) {
@@ -129,6 +67,27 @@ function resetInside() {
     }
 }
 
+function createMenu() {
+
+    btn1 = createButton('Reset opisów');
+    btn1.mouseClicked(resetBounds);
+    select('.btL').child(btn1);
+
+    btn2 = createButton('Reset planszy');
+    btn2.mouseClicked(resetInside);
+    select('.btR').child(btn2);
+
+    skalar = createSlider(60, 140, 60, 10);
+    select('.sld').child(skalar);
+    skalar.changed(resize);
+}
+
+function resize() {
+    size = skalar.value();
+    print('done!');
+    resizeCanvas(size * 6, size * 6)
+}
+
 function setup() {
     createMenu();
     size = skalar.value();
@@ -140,27 +99,20 @@ function setup() {
     for (let i = 0; i < 6; i++) {
 
         for (let j = 0; j < 6; j++) {
+            if (n != 1 && n != 6 && n != 31 && n != 36) {
 
-            if (n == 31) {
-                segments.push(new Btn(j, i, 'Reset oznaczeń', 'o'))
-            } else if (n == 36) {
-                segments.push(new Btn(j, i, 'Reset planszy', 'p'))
-            } else {
-
-                if (n != 1 && n != 6) {
-                    if (bound.includes(n)) {
-                        segments.push(new Segment(j, i, color(211, 211, 211), false, true, false));
-                        bd.push(segments.length - 1);
-                    } else {
-                        segments.push(new Segment(j, i, color(255, 160, 122), true, false, true));
-                        ins.push(segments.length - 1);
-                    }
+                if (bound.includes(n)) {
+                    segments.push(new Segment(j, i, color(211, 211, 211), false));
+                    bd.push(segments.length - 1);
+                } else {
+                    segments.push(new Segment(j, i, color(255, 160, 122), true));
+                    ins.push(segments.length - 1);
                 }
-
             }
             n++;
         }
     }
+    z = true;
 }
 
 function draw() {
@@ -181,7 +133,7 @@ function draw() {
 function mouseClicked() {
     for (s of segments) {
         if (s.checkPointing()) {
-            s.action();
+            s.increaseNum();
         }
     }
 }
